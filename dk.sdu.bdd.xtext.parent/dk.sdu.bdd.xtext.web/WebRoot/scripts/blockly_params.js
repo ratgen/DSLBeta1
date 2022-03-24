026cc52849221bbb3cf17c4b010dc0018387dda0
joakimbetzer <43139396+joakimbetzer@users.noreply.github.com>
2022-03-24 10:56:25 +0100

    InsertingCustomBlocks

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/blockly_params.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/blockly_params.js
@@ -1,34 +1,156 @@
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "scenarioblock",
+        "message0": "Scenario %1",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "NAME",
+            "check": "String"
+          }
+        ],
+        "inputsInline": true,
+        "nextStatement": null,
+        "colour": 0,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "type": "givenblock",
+        "message0": "Given %1",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "giveninput"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 45,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "type": "whenblock",
+        "message0": "When %1 %2",
+        "args0": [
+          {
+            "type": "field_dropdown",
+            "name": "whenwhat",
+            "options": [
+              [
+                "I",
+                "INAME"
+              ],
+              [
+                "the",
+                "THENAME"
+              ]
+            ]
+          },
+          {
+            "type": "input_value",
+            "name": "wheninput"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 90,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "type": "thenblock",
+        "message0": "Then %1",
+        "args0": [
+          {
+            "type": "input_value",
+            "name": "theninput"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "colour": 135,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "type": "whichmeansblock",
+        "message0": "Which means %1 %2",
+        "args0": [
+          {
+            "type": "input_dummy"
+          },
+          {
+            "type": "input_statement",
+            "name": "NAME"
+          }
+        ],
+        "inputsInline": true,
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 345,
+        "tooltip": "",
+        "helpUrl": ""
+      },
+      {
+        "type": "textblock",
+        "message0": "%1",
+        "args0": [
+          {
+          "type": "field_input",
+          "name": "TEXT",
+        }
+      ],
+        "output": "String",
+        "style": "text_blocks",
+      }
+  ]);
+ 
+
 var toolbox = {
-  "kind": "flyoutToolbox",
+  "kind": "categoryToolbox",
   "contents": [
     {
-      "kind": "block",
-      "type": "controls_if"
+      "kind": "category",
+      "name": "Bdd blocks",
+      "contents": [
+        {
+          "kind": "block",
+          "type": "scenarioblock"
+        },
+        {
+          "kind": "block",
+          "type": "givenblock"
+        },
+        {
+          "kind": "block",
+          "type": "whichmeansblock"
+        },
+        {
+          "kind": "block",
+          "type": "whenblock"
+        },
+        {
+          "kind": "block",
+          "type": "thenblock"
+        }
+      ]
     },
     {
-      "kind": "block",
-      "type": "controls_repeat_ext"
-    },
-    {
-      "kind": "block",
-      "type": "logic_compare"
-    },
-    {
-      "kind": "block",
-      "type": "math_number"
-    },
-    {
-      "kind": "block",
-      "type": "math_arithmetic"
-    },
-    {
-      "kind": "block",
-      "type": "text"
-    },
-    {
-      "kind": "block",
-      "type": "text_print"
-    },
+      "kind": "category",
+      "name": "Text blocks",
+      "contents": [
+        {
+          "kind": "block",
+          "type": "textblock"
+        }
+      ]
+    }
   ]
 }
+
+
 var workspace = Blockly.inject('blockly-editor', {toolbox: toolbox});
