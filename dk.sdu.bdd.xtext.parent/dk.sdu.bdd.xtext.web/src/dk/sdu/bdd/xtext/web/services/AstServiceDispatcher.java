00b7f0d6b3576e9a7bd0fec2c653348acabd2fe2
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-05-08 15:03:07 +0200

    Work on ast

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/src/dk/sdu/bdd/xtext/web/services/AstServiceDispatcher.java
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/src/dk/sdu/bdd/xtext/web/services/AstServiceDispatcher.java
@@ -1,19 +1,17 @@
 package dk.sdu.bdd.xtext.web.services;
 
-import java.util.Set;
-
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.web.server.IServiceContext;
-import org.eclipse.xtext.web.server.ISession;
 import org.eclipse.xtext.web.server.InvalidRequestException;
 import org.eclipse.xtext.web.server.XtextServiceDispatcher;
 import org.eclipse.xtext.web.server.model.IWebResourceSetProvider;
 
 import dk.sdu.bdd.xtext.bddDsl.StatePhrase;
+import dk.sdu.bdd.xtext.bddDsl.impl.ScenarioImpl;
 import dk.sdu.bdd.xtext.bddDsl.impl.ScenarioStateImpl;
 
 import com.google.inject.Inject;
@@ -82,6 +80,21 @@ public class AstServiceDispatcher extends XtextServiceDispatcher {
 					System.out.println(" entity " + stat.getEntity());
 					System.out.println(" propery " + stat.getProperty());
 				}
+			}
+			if(cont.getClass() == ScenarioImpl.class) {
+				ScenarioImpl scenario = (ScenarioImpl) cont;
+				System.out.println(scenario.getName());
+				eContentExplorer(scenario.getPreStateE());
+				eContentExplorer(scenario.getPreState());
+				eContentExplorer(scenario.getPostState());
+				eContentExplorer(scenario.getActionE());
+				eContentExplorer(scenario.getPreState1());
+				eContentExplorer(scenario.getAction1());
+				eContentExplorer(scenario.getPostState1());
+				eContentExplorer(scenario.getPreStateE1());
+				eContentExplorer(scenario.getPreState2());
+				eContentExplorer(scenario.getAction2());
+				eContentExplorer(scenario.getPostState2());
 			}
 			if (cont.eContents().size() != 0) {
 				eContentExplorer(cont);
