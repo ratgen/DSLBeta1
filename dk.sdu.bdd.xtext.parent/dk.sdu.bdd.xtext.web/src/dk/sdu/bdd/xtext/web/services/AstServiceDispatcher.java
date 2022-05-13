a94b4d6e118eee06fad60f5b2a984d0024faadad
Peter Heilbo Ratgen <peter@pratgen.dk>
2022-05-14 01:24:31 +0200

    Working on creating blocks

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/src/dk/sdu/bdd/xtext/web/services/AstServiceDispatcher.java
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/src/dk/sdu/bdd/xtext/web/services/AstServiceDispatcher.java
@@ -1,23 +1,46 @@
 package dk.sdu.bdd.xtext.web.services;
 
+import java.util.ArrayList;
+
 import org.eclipse.emf.common.util.EList;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.web.server.IServiceContext;
 import org.eclipse.xtext.web.server.InvalidRequestException;
 import org.eclipse.xtext.web.server.XtextServiceDispatcher;
 import org.eclipse.xtext.web.server.model.IWebResourceSetProvider;
-import org.eclipse.xtext.nodemodel.ICompositeNode;
-import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.eclipse.xtext.AbstractElement;
+import org.eclipse.xtext.AbstractRule;
+import org.eclipse.xtext.Alternatives;
+import org.eclipse.xtext.Assignment;
+import org.eclipse.xtext.CrossReference;
+import org.eclipse.xtext.Group;
+import org.eclipse.xtext.Keyword;
+import org.eclipse.xtext.ParserRule;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.impl.AlternativesImpl;
+import org.eclipse.xtext.impl.AssignmentImpl;
+import org.eclipse.xtext.impl.GroupImpl;
+import org.eclipse.xtext.impl.KeywordImpl;
+import org.eclipse.xtext.impl.ParserRuleImpl;
+import org.eclipse.xtext.impl.RuleCallImpl;
+
+import dk.sdu.bdd.xtext.services.BddDslGrammarAccess;
 
 import com.google.inject.Inject;
 
 public class AstServiceDispatcher extends XtextServiceDispatcher {
 	@Inject
 	private IWebResourceSetProvider resourceSetProvider;
+	
+	@Inject
+	private BddDslGrammarAccess grammarAccess;
 		
 	@Override
 	protected ServiceDescriptor createServiceDescriptor(String serviceType, IServiceContext context){
@@ -45,28 +68,94 @@ public class AstServiceDispatcher extends XtextServiceDispatcher {
 			System.out.println("Resource  URI: " + uri);
 			EList<EObject> objectContents = item.getContents();
 			System.out.println("item contents " + objectContents);
-			for (EObject obj : objectContents) {
-				System.out.println("EObject_string: " + obj.toString());
+			//for (EObject obj : objectContents) {
+				//System.out.println("EObject_string: " + obj.toString());
 				
 				
-				System.out.println(dump(obj, "   "));
+				//System.out.println(dump(obj, "   "));
+			//}
+		}
+		JSONArray blockArray = new JSONArray();
+
+		EList<AbstractRule> rules = grammarAccess.getGrammar().getRules();
+		for (AbstractRule rule : rules) {
+			System.out.println("rule: " + rule.getName());
+			if (rule.getClass() == ParserRuleImpl.class) {
+				ParserRule parserRule = (ParserRule) rule;
+					
+				JSONObject block = parseRule(parserRule);
+				blockArray.add(block);
+				System.out.println(block);
+				System.out.println("rule contents: \n" + dump(rule, "    ")); 
 			}
-			System.out.println();
-			System.out.println();
 		}
 		
 		
 		ServiceDescriptor serviceDescriptor = new ServiceDescriptor();		
 		serviceDescriptor.setService(() -> {
-	        return new AstServiceResult(resource);
+	        return new AstServiceResult(blockArray.toJSONString());
 	      });
 		return serviceDescriptor;
+	}
+	
+	//Parse a rule and return a Blockly Block representing the Rule
+	private JSONObject parseRule(ParserRule rule) {
+		JSONObject json = new JSONObject();
+		
+		parseLoop(rule);
+		
+		return json;
+	}
+	
+	private JSONObject parseLoop(EObject obj) {
+		if (obj.getClass() == GroupImpl.class) {
+			
+			obj.eContents().forEach(
+					(item) -> {parseLoop(item);}
+			);
+		}
+		if (obj.getClass() == KeywordImpl.class) {
+					
+				}
+		if (obj.getClass() == AssignmentImpl.class) {
+			
+		}
+		if (obj.getClass() == RuleCallImpl.class) {
+			
+		}
+		if (obj.getClass() == AlternativesImpl.class) {
+			
+		}
+		if (obj.getClass() == CrossReference.class) {
+		}
+		return null;
+	}
+	
+	
+	private EList<EObject> parseGroup(Group group){
+		return group.eContents();
+	}
+	
+	private String parseKeyword(Keyword keyword) {
+		return keyword.getValue();
+	}
+	
+	private String parseAssignment(Assignment assignment) {
+		for (EObject rule : assignment.eContents()) {
+			if(rule.getClass() == RuleCallImpl.class) {
+				RuleCall call = (RuleCall) rule;
+				ParserRule ref = (ParserRule) call.eCrossReferences().get(0);
+				return ref.getName();
+			}
+		}
+		return "";
 	}
 	
 	private static String dump(EObject mod_, String indent) {
 		
 	    var res = indent + mod_.toString().replaceFirst(".*[.]impl[.](.*)Impl[^(]*", "$1 ");
-
+	    
+	   
 	    for (EObject a :mod_.eCrossReferences()) {
 	        res +=  "->" + a.toString().replaceFirst(".*[.]impl[.](.*)Impl[^(]*", "$1 ");
 	    }
@@ -74,8 +163,7 @@ public class AstServiceDispatcher extends XtextServiceDispatcher {
 	    for (EObject f :mod_.eContents()) {
 	        res += dump(f, indent+"    ");
 	    }
-	    ICompositeNode node =  NodeModelUtils.getNode(mod_);
-		res += "\n " + node.getText() + "\n";
+	    
 	    return res;
 	}
 }
