352a4950243c909309534b0c5917aed375ddb106
mustafa <mahme19@student.sdu.dk>
2022-05-30 17:16:04 +0200

    Scenario count limit to 1
    fixed

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.examples/src/dk/sdu/bdd/xtext/examples/features/logic/fileReader.py
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.examples/src/dk/sdu/bdd/xtext/examples/features/logic/fileReader.py
@@ -1,24 +1,30 @@
 
 
 def readFile():
+
+    scenarioCounter = 0
     with open('sample.bdd') as f:
         fileWriter = open("features/tests.feature","w")
         content = f.readlines()
         disallowedStrings = ['actions','states', 'properties','}' ,'/*','*/', "which means", '//', 'declarative', 'imperative', 'model'  ]
-
         for i in range(0,len(content)):
+            if scenarioCounter == 2:
+                break
             line = content[i]
             if any(x in line for x in disallowedStrings):
                 pass
             elif(line.startswith('Scenario:')):
-                
+                scenarioCounter += 1
+                if(scenarioCounter == 2):
+                    break
                 scenario = line.replace('"',"").replace("Scenario: ","")
                 s = "Feature: " + scenario
                 fileWriter.write(s)
             elif(line.startswith('Given') |line.startswith('When') | line.startswith('Then') ):
-            
+                
                 s = '\n'+'\tScenario: '+ line
                 fileWriter.write(s)
             else:
+                
                 fileWriter.write(line)
         fileWriter.close()
