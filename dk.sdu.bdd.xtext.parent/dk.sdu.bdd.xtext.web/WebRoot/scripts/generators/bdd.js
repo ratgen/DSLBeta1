1a860dc652d8ae9e5755b1260857dd3c35c73942
ozgey99 <ozgey99@gmail.com>
2024-02-13 10:05:22 +0100

    Added some of the declarative entity definition block generators

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/generators/bdd.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/generators/bdd.js
@@ -23,6 +23,47 @@ bddGenerator.forBlock['ModelRef'] = function(block, generator) {
     return code;
 };
 
+// Declarative Entities and Definitions
+bddGenerator.forBlock['DeclarativeEntityDef'] = function(block, generator) {
+    const name = generator.valueToCode(block, 'name_ID',
+        Order.ATOMIC);
+
+    const input2 = generator.statementToCode(block, 'DeclarativeEntityDef_input_2');
+    const input3 = generator.statementToCode(block, 'DeclarativeEntityDef_input_3');
+
+    const code = `declarative entity ${name}\n${input2}\n{${input3}}`;
+    return code;
+};
+
+bddGenerator.forBlock['subBlock_DeclarativeEntityDef_is'] = function(block, generator) {
+    const name = generator.valueToCode(block, 'subBlock_DeclarativeEntityDef_is_ID',
+        Order.ATOMIC);
+
+    const input = generator.statementToCode(block, 'subBlock_DeclarativeEntityDef_is_input_2');
+
+    const code = `is ${name}\n${input}`;
+    return code;
+};
+
+bddGenerator.forBlock['subBlock_subBlock_DeclarativeEntityDef_is_,'] = function(block, generator) {
+    const name = generator.valueToCode(block, 'subBlock_subBlock_DeclarativeEntityDef_is_,_ID',
+        Order.ATOMIC);
+    const code = `, ${name}`;
+    return code;
+};
+
+// bddGenerator.forBlock['subBlock_DeclarativeEntityDef_actions'] = function(block, generator) {
+    
+// };
+
+// bddGenerator.forBlock['subBlock_DeclarativeEntityDef_states'] = function(block, generator) {
+    
+// };
+
+// bddGenerator.forBlock['subBlock_DeclarativeEntityDef_properties'] = function(block, generator) {
+    
+// };
+
 // Terminals
 bddGenerator.forBlock['STRING'] = function(block) {
     const textValue = block.getFieldValue('TEXT_INPUT');
