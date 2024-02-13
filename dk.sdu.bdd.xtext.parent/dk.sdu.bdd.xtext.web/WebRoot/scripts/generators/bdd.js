9e8308e7903ac34d5407031f4c2aa601deb863cc
ozgey99 <ozgey99@gmail.com>
2024-02-13 09:28:30 +0100

    Model and using block generators

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/generators/bdd.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/generators/bdd.js
@@ -4,6 +4,26 @@ const Order = {
 
 bddGenerator = new Blockly.Generator('bdd');
 
+// Model
+bddGenerator.forBlock['Model'] = function(block, generator) {
+    const modelName = generator.valueToCode(block, 'name_ID',
+        Order.ATOMIC);
+
+    const statementMembers =
+        generator.statementToCode(block, 'alternatives_statement'); // like an object
+
+    const code = `model ${modelName}\n${statementMembers}`;
+    return code;
+};
+
+bddGenerator.forBlock['ModelRef'] = function(block, generator) {
+    const modelName = generator.valueToCode(block, 'ModelRef_ID',
+        Order.ATOMIC);
+    const code = `using ${modelName}`;
+    return code;
+};
+
+// Terminals
 bddGenerator.forBlock['STRING'] = function(block) {
     const textValue = block.getFieldValue('TEXT_INPUT');
     const code = `"${textValue}"`;
