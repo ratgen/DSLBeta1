87e2920eaf35856dc72f68e227ab696dccf45720
ozgey99 <ozgey99@gmail.com>
2024-03-10 14:48:57 +0100

    try to usse previous block

--- dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/generators/blockGenerator.js
+++ dk.sdu.bdd.xtext.parent/dk.sdu.bdd.xtext.web/WebRoot/scripts/generators/blockGenerator.js
@@ -1,4 +1,5 @@
 let blockDefinitions;
+let previousBlock;
 
 function generateBlocksFromAst(ast, workspace, blockArray) {
     if (!workspace || !blockArray)
@@ -8,6 +9,7 @@ function generateBlocksFromAst(ast, workspace, blockArray) {
         blockDefinitions = blockArray;
     
     workspace.clear();
+    previousBlock = null;
     generateBlocks(ast, workspace, null);
     workspace.render();
 }
@@ -84,27 +86,59 @@ function addBlockToWorkspace(parsedObj, workspace, parentBlock) {
         var parentBlockDefinition = blockDefinitions.find(function(b) {
             return b.type === parentBlock.type;
         });
+        
+        try { // try to add the input to the correct place
+            var previousBlockDefinition = null;  
+            var inputArgument = null;  
 
-        var inputArgument = parentBlockDefinition.args0.find(function(a) {
-            return a.check.some(function(checkItem) {
-                return checkItem.includes(substringToSearch);
-            }) && a.type === 'input_statement';
-        });
+            if (previousBlock) // connect as a subblock
+            {
+                previousBlockDefinition = blockDefinitions.find(function(b) {
+                    return b.type === previousBlock.type;
+                });
 
-        if (inputArgument) // means we can connect to the previous block as an input
-        {
+                inputArgument = previousBlockDefinition.args0.find(function(a) {
+                    return a.check.some(function(checkItem) {
+                        return checkItem.includes(substringToSearch);
+                    }) && a.type === 'input_statement';
+                });
+
+                if (!inputArgument) { // try going to the outer level
+                    previousBlock = previousBlock.getPreviousBlock();
+
+                    previousBlockDefinition = blockDefinitions.find(function(b) {
+                        return b.type === previousBlock.type;
+                    });
+    
+                    inputArgument = previousBlockDefinition.args0.find(function(a) {
+                        return a.check.some(function(checkItem) {
+                            return checkItem.includes(substringToSearch);
+                        }) && a.type === 'input_statement';
+                    });
+                }
+            }
+
+            if (inputArgument) // means we are connecting to the previous block as subblock
+            {
+                parentBlock = previousBlock;
+            }
+            else // means we have to connect to the parent instead
+            {
+                inputArgument = parentBlockDefinition.args0.find(function(a) {
+                    return a.check.some(function(checkItem) {
+                        return checkItem.includes(substringToSearch);
+                    }) && a.type === 'input_statement';
+                });
+            }
+            
             var blockType = inputArgument.check.find(function(c) {
                 return c.includes(substringToSearch);
             });
 
             blockToAdd = workspace.newBlock(blockType);
-            blockDefinition = blockDefinitions.find(function(b) {
-                return b.type === blockType;
-            });
         }
-        else 
-        {
-            console.log("Can't add block as an input argument.");
+        catch(e) {
+            console.log(e);
         }
     }
 
@@ -116,6 +150,8 @@ function addBlockToWorkspace(parsedObj, workspace, parentBlock) {
 
     if (parentBlock)
         addParentBlock(parentBlock, blockToAdd, workspace);
+
+    previousBlock = blockToAdd;
 
     workspace.getBlockById(blockToAdd.id).initSvg();
     return blockToAdd;
